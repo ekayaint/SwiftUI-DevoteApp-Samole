bc86848229646c271603be2c84e38a8c6976abdc
ekayaint <ekayaint@gmail.com>
2023-08-31 15:39:34 +0300

    started

---
+++ SwiftUI-DevoteApp-Samole/Utility/Constant.swift
@@ -0,0 +1,23 @@
+//
+//  Constant.swift
+//  SwiftUI-DevoteApp-Samole
+//
+//  Created by ekayaint on 31.08.2023.
+//
+
+import SwiftUI
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+
+// MARK: - UI
+
+
+// MARK: - UX
